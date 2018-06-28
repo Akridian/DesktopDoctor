86add172ec9a2a777a49f5ce84fdbf8d7a7c11e9
Akridian <pavel.ul@mail.ru>
2018-06-28 14:12:26 +0400

    Authorization works
    Looks ugly

--- DesktopDoctor/AuthorizationForm.Designer.cs
+++ DesktopDoctor/AuthorizationForm.Designer.cs
@@ -28,29 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.toPatientsButton = new System.Windows.Forms.Button();
-            this.toMedicinesButton = new System.Windows.Forms.Button();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // toPatientsButton
+            // loginTextBox
             // 
-            this.toPatientsButton.Location = new System.Drawing.Point(78, 379);
-            this.toPatientsButton.Name = "toPatientsButton";
-            this.toPatientsButton.Size = new System.Drawing.Size(99, 23);
-            this.toPatientsButton.TabIndex = 0;
-            this.toPatientsButton.Text = "Пациенты";
-            this.toPatientsButton.UseVisualStyleBackColor = true;
-            this.toPatientsButton.Click += new System.EventHandler(this.ToPatientsButton_Click);
+            this.loginTextBox.Location = new System.Drawing.Point(341, 115);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(100, 20);
+            this.loginTextBox.TabIndex = 2;
             // 
-            // toMedicinesButton
+            // passwordTextBox
             // 
-            this.toMedicinesButton.Location = new System.Drawing.Point(593, 379);
-            this.toMedicinesButton.Name = "toMedicinesButton";
-            this.toMedicinesButton.Size = new System.Drawing.Size(107, 23);
-            this.toMedicinesButton.TabIndex = 1;
-            this.toMedicinesButton.Text = "Медикаменты";
-            this.toMedicinesButton.UseVisualStyleBackColor = true;
-            this.toMedicinesButton.Click += new System.EventHandler(this.ToMedicinesButton_Click);
+            this.passwordTextBox.Location = new System.Drawing.Point(341, 171);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox.TabIndex = 3;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(341, 225);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(100, 23);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Войти";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Для админа: admin - admin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(487, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Для врача: vrach - vrach";
             // 
             // AuthorizationForm
             // 
@@ -58,19 +84,25 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.ControlBox = false;
-            this.Controls.Add(this.toMedicinesButton);
-            this.Controls.Add(this.toPatientsButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginTextBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AuthorizationForm";
             this.Text = "AuthorizationForm";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button toPatientsButton;
-        private System.Windows.Forms.Button toMedicinesButton;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- DesktopDoctor/AuthorizationForm.cs
+++ DesktopDoctor/AuthorizationForm.cs
@@ -12,29 +12,52 @@ namespace DesktopDoctor
 {
     public partial class AuthorizationForm : Form
     {
+        DesktopDoctorDatabaseEntities db = new DesktopDoctorDatabaseEntities();
+
         public AuthorizationForm()
         {
             InitializeComponent();
         }
 
-        private void ToPatientsButton_Click(object sender, EventArgs e)
+        private void LoginButton_Click(object sender, EventArgs e)
         {
-            PatientsForm patientsForm = new PatientsForm
+            string login = loginTextBox.Text.ToString();
+            string password = passwordTextBox.Text.ToString();
+            try
             {
-                MdiParent = MdiParent
-            };
-            patientsForm.Show();
-            Close();
-        }
-
-        private void ToMedicinesButton_Click(object sender, EventArgs e)
-        {
-            MedicinesForm medicinesForm = new MedicinesForm
+                Account account = db.Accounts.Where(acc => acc.Login == login).Select(acc => acc).First();
+                if (account.Password == password)
+                {
+                    (MdiParent as MainForm).employee = account.Employee;
+                    if (account.SecurityLevel.Code == "doctor")
+                    {
+                        PatientsForm patientsForm = new PatientsForm
+                        {
+                            MdiParent = MdiParent
+                        };
+                        patientsForm.Show();
+                        Close();
+                    }
+                    else if (account.SecurityLevel.Code == "admin")
+                    {
+                        MedicinesForm medicinesForm = new MedicinesForm
+                        {
+                            MdiParent = MdiParent
+                        };
+                        medicinesForm.Show();
+                        Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный пароль");
+                }
+            }
+            catch (InvalidOperationException)
             {
-                MdiParent = MdiParent
-            };
-            medicinesForm.Show();
-            Close();
+                MessageBox.Show("Аккаунт не найден");
+            }
+            
         }
     }
 }
